12c09acdbfa3f1f0df2d578b01f5967c9ad62191
Harsh Chilhate <abitharshchilhatel@chilhate.com>
2025-11-21 16:32:54 +0530

    Now game run on fullscreen. Press q key to exit game. The Ship can move Left and Right.

--- settings.py
+++ settings.py
@@ -2,7 +2,11 @@ class Settings:
     #a class to store all the settings for the alien invasion
     def __init__(self):
         #initialize the game settings
+
         #Screen settings
         self.screen_width = 1080
         self.screen_height = 720
         self.bg_colour = (230,230,230)
+
+        #ship settings
+        self.ship_speed = 1.5
