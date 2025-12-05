63b6a7e52232eb81a72531ac4bb27527ebb3d179
Harsh Chilhate <abitharshchilhatel@chilhate.com>
2025-12-05 15:23:03 +0530

    Alien fleet added

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     #overall class to manage game assets and behaviour
@@ -18,9 +19,10 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
 
-        #set background colour 
-        self.bg_colour = (230,230,230)
+        self._create_fleet()
+
         
     def run_game(self):
         #start the main looop for the game
@@ -66,6 +68,34 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+
+    def _create_fleet(self):
+        """Create the fleet of the alien"""
+        #create an alien and keep adding aliens until there's no room left
+        #space between aliens is one alien width and one alien height
+        alien = Alien(self)
+        alien_width ,alien_height  = alien.rect.size
+
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+
+            #finsihed a row ; reset x vlaue, and increment y value
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+
+    def _create_alien(self, x_position, y_position):
+        """crete an alien and place it in the fleet"""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
+
                      
 
     def _update_screen(self):
@@ -74,6 +104,7 @@ class AlienInvasion:
             for bullet in self.bullets.sprites():
                 bullet.draw_bullet()
             self.ship.blitme()
+            self.aliens.draw(self.screen)
              
             pygame.display.flip()
          
